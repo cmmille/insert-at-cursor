3518c1bfc57b2195c118fbb920b5a6897228b976
cmmille <cmmiller.bio@gmail.com>
2023-08-11 12:01:14 -0700

    Prevent example from being built

--- example/index.js
+++ example/index.js
@@ -1,4 +1,4 @@
-import { TextAreaSelectUtility } from "../src/index.ts";
+import { TextAreaSelectUtility } from "../src/index";
 
 const TextAreaTools = new TextAreaSelectUtility("catInfo", "catFacts");
 window.TextAreaTools = TextAreaTools;
